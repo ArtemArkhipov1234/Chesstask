09af8d5e0c74c6596346610920814ab3fe0abb7e
ArtemArkhipov <aarkhipov2005@gmail.com>
2020-10-09 08:17:34 +0700

    Initial Commit

--- task1.py
+++ task1.py
@@ -1,32 +1,20 @@
-import math
-counter = 0
-cost = 0
-expense = 1
-while counter < 64:
-    counter = counter + 1
-    cost = cost + expense
-    expense = expense * 2
-print('Правитель должен был заплатить изобретателю', cost, 'зёрен')
+a = 100000
+while a <= 999999:
+    counter = 0
+    n1 = a
+    n2 = a + 1
+    n3 = a + 2
+    n4 = a + 3
+    if n1 % 10 == n1 // 10000 % 10 and n1 // 1000 % 10 == n1 // 10 % 10:
+        counter += 1
+    if n2 % 10 == n2 // 10000 % 10 and n2 // 1000 % 10 == n2 // 10 % 10:
+        counter += 1
+    if n3 // 10000 % 10 == n3 // 10 % 10 and n3 // 1000 % 10 == n3 // 100 % 10:
+        counter += 1
+    if n4 // 10000 % 10 == n4 // 10 % 10 and n4 // 1000 % 10 == n4 // 100 % 10 and n4 // 100000 == n4 % 10:
+        counter += 1
+    if counter == 4:
+        print(a)
+    a += 1
 
-print(cost / 2e8, 'тонн')
-# Вес пшеницы в тоннах
-
-cornArea = float(input()) / 2
-countryArea = 17100000
-regionArea = 178200
-cityArea = 502.7
-print(cost / cornArea // countryArea, 'слоёв')
-# Количество слоёв зёрен для страны
-print(cost / cornArea // regionArea, 'слоёв')
-# Количество слоёв зёрен для области
-print(cost / cornArea // cityArea, 'слоёв')
-# Количество слоёв зёрен для города
-
-cornCountryAmountPerYear = 26.6
-cornRegionAmountPerYear = 18.3
-cornCityAmountPerYear = 3.8
-
-print('Потребовалось бы', math.ceil(cost / cornCountryAmountPerYear), 'лет')
-print('Потребовалось бы', math.ceil(cost / cornRegionAmountPerYear), 'лет')
-print('Потребовалось бы', math.ceil(cost / cornCityAmountPerYear), 'лет')
 
